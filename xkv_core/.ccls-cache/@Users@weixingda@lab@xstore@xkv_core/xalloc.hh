284daa607012ec362bce86ae7cafb9453e810eb4
wxd <wxdwfc@gmail.com>
2020-06-03 21:32:58 +0800

    minor scheme

--- xkv_core/.ccls-cache/@Users@weixingda@lab@xstore@xkv_core/xalloc.hh
+++ xkv_core/.ccls-cache/@Users@weixingda@lab@xstore@xkv_core/xalloc.hh
@@ -1,24 +1,22 @@
 #pragma once
 
-#include "../deps/r2/src/common.hh"
+#include "./alloc_trait.hh"
 
 namespace xstore {
 
 namespace xkv {
 
-using namespace r2;
-
 // a slab allocator
 // S indicates the allocate memory block sz
-template <usize S> class XAlloc {
+template <usize S> class XAlloc : public AllocTrait<XAlloc> {
   char *mem_pool = nullptr;
   const usize total_sz = 0;
 
 public:
   // interfaces
-  auto alloc() -> Option<char *> { return {}; }
+  auto alloc_impl() -> Option<char *> { return {}; }
 
-  auto dealloc(char *data) { ASSERT(false) << "not implemented"; }
+  auto dealloc_impl(char *data) { ASSERT(false) << "not implemented"; }
 };
 } // namespace xkv
 
