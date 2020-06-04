7947682f673df78d2c65ea2687203f039acc6443
wxd <wxdwfc@gmail.com>
2020-06-04 14:43:46 +0800

    oops, add missing file

---
+++ xkv_core/src/xtree/spin_lock.hh
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "../../../deps/r2/src/common.hh"
+
+// handy atomic builtins come from leveldb
+#include "../../../deps/port/atomic.hpp"
+
+namespace xstore {
+
+namespace xkv {
+
+namespace xtree {
+
+using namespace r2;
+
+// a compact spinlock without padding
+struct CompactSpinLock {
+  volatile u16 guard = 0;
+
+  CompactSpinLock() = default;
+
+  inline void lock() {
+    while (1) {
+      if (!xchg16((u16 *)(&guard), 1))
+        return;
+
+      while (guard) {
+        ::r2::relax_fence();
+      }
+    }
+  }
+
+  inline void unlock() {
+    r2::compile_fence();
+    this->guard = 0;
+  }
+
+  inline u16 try_lock() { return xchg16((u16 *)(&guard), 1); }
+
+  inline u16 is_locked() const { return guard; }
+};
+
+} // namespace xtree
+} // namespace xkv
+} // namespace xstore
