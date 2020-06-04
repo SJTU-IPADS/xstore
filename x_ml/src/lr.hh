eae720c70fdb351964bad917c6b0a01d58d50c90
wxd <wxdwfc@gmail.com>
2020-06-04 21:43:39 +0800

    xml scheme

---
+++ x_ml/src/lr.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "./ml_trait.hh"
+
+namespace xstore {
+
+namespace xml {
+
+/*!
+  Core LR Model, leverage MKL for training
+ */
+struct LR {
+  // TODO: impl
+};
+
+} // namespace xml
+} // namespace xstore
