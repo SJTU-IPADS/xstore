0dc82fe57738286ac69487e92c750092335f71e4
wxd <wxdwfc@gmail.com>
2020-06-05 20:15:37 +0800

    minor test nn

--- x_ml/tests/test_nn.cc
+++ x_ml/tests/test_nn.cc
@@ -1,6 +1,8 @@
-#pragma once
+#include <gtest/gtest.h>
 
-#include "tiny_dnn/tiny_dnn.h"
+#include "../../deps/kvs-workload/static_loader.hh"
+#include "../../deps/tiny-dnn/tiny_dnn/tiny_dnn.h"
+#include "../../deps/r2/src/common.hh"
 
 using namespace tiny_dnn;
 using namespace tiny_dnn::activation;
@@ -8,12 +10,18 @@ using namespace tiny_dnn::layers;
 
 namespace test {
 
+using namespace kvs_workloads;
+
 TEST(XML, NN) {
   network<sequential> net;
 
-  net << fc(32 * 32, 300) << sigmoid() << fc(300, 10);
+  net << fc(1 * 32, 300) << sigmoid() << fc(300, 1);
 
-  ASSERT_EQ(net.in_data_size(),32 * 32);
+  std::vector<u64> train_set;
+  std::vector<u64> label;
+
+  auto data = StaticLoader::load_from_file("./test_dataset.txt");
+  LOG(4) << "total: " << data->size() << " loaded";
 }
 
 } // namespace test
