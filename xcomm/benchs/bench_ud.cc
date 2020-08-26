964475fdb099936bebe78c33ae7c66bac0d172d0
wxd <wxdwfc@gmail.com>
2020-08-26 11:17:16 +0800

    minor template for ud support

---
+++ xcomm/benchs/bench_ud.cc
@@ -0,0 +1,5 @@
+#include "../src/transport/rdma_ud_t.hh"
+
+#include <iostream>
+
+int main() { std::cout << "hello world!"; }
---
+++ xcomm/src/transport/rdma_ud_t.hh
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "./trait.hh"
+
+namespace xstore {
+
+namespace transport {
+// TODO: not implemented
+}
+
+} // namespace xstore
