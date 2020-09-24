527281df024a27ec26f1e16d7cc91506c228b76e
wxd <wxdwfc@gmail.com>
2020-09-24 19:44:34 +0800

    add count line

---
+++ count_line.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+find xcache x_ml xkv_core/ xcomm xutils ! -path '*/deps/*' \
+    | egrep '\.php|\.as|\.sql|\.css|\.js|\.cc|\.h|.cpp|.c ' \
+    | grep -v '\.svn' | xargs cat | sed '/^\s*$/d' \
+    | wc -l
