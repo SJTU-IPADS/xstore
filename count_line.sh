d6271691d5881d2b5d4a95115c6b0917b131cd04
wxd <wxdwfc@gmail.com>
2020-09-25 15:47:28 +0800

    minor

--- count_line.sh
+++ count_line.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find xcache x_ml xkv_core/ xcomm xutils ! -path '*/deps/*' \
+find benchs xcache x_ml xkv_core/ xcomm xutils ! -path '*/deps/*' \
     | egrep '\.php|\.as|\.sql|\.css|\.js|\.cc|\.h|.cpp|.c ' \
     | grep -v '\.svn' | xargs cat | sed '/^\s*$/d' \
     | wc -l
